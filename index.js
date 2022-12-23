94c47ef5d18c14dd387f6fdecfcbe71d75b7c9ba
Denny Marc Maquiling <dennymarc.maquiling@fligno.com>
2022-12-23 23:29:44 +0800

    backend

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const home = require("./routes/home");
 // Middlewares
 const app = express();
 app.use(express.json());
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
 
+mongoose.connect(
+  "mongodb+srv://cloudmongodbuser1:cloudmongodbpassword143@cluster0.gavsumi.mongodb.net/ad-lib-io?retryWrites=true&w=majority"
+);
 // Routes
 app.use("/home", home);
 
