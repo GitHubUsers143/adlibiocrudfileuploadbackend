0a3a0d5002208c5ad6bfef9b88d0827143bf6d04
Denny Marc Maquiling <dennymarc.maquiling@fligno.com>
2022-12-23 23:38:51 +0800

    backend

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ app.get("/", (req, res) => {
   res.send("Express is here");
 });
 
+app.use("/users/login", require("./routes/user"));
+// app.use("/", require("./routes/user"));
+// app.use("/users", require("./routes/user"));
+// app.use("/users/:id", require("./routes/user"));
+// app.use("/users/update/:id", require("./routes/user"));
+// app.use("/users/delete/:id", require("./routes/user"));
+
 app.listen("9001", function () {
   console.log("Server is running");
 });
